53622620824f960132b3196b58de3c71982cce4d
Martin Helmich <kontakt@martin-helmich.de>
2018-04-15 17:05:32 +0200

    Use correct include path when running as imported package

--- cmd/s2c.php
+++ cmd/s2c.php
@@ -3,9 +3,9 @@
 
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
-} else if (file_exists(__DIR__ . '/../../autoload.php')) {
+} else if (file_exists(__DIR__ . '/../../../autoload.php')) {
     /** @noinspection PhpIncludeInspection */
-    require_once __DIR__ . '/../../autoload.php';
+    require_once __DIR__ . '/../../../autoload.php';
 } else {
     die('Could not find an autoload.php. Did you set up all dependencies?');
 }
