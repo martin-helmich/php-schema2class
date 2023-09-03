f1a09c0f31db8ad3436c694c9c837c3ff7c034c2
Martin Helmich <m.helmich@mittwald.de>
2023-09-03 15:06:09 +0200

    Add better support for unspecific "number" types

---
+++ src/Generator/Property/NumberProperty.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+namespace Helmich\Schema2Class\Generator\Property;
+
+use Composer\Semver\Semver;
+
+class NumberProperty extends AbstractProperty
+{
+    use TypeConvert;
+
+    public static function canHandleSchema(array $schema): bool
+    {
+        if (!isset($schema["type"])) {
+            return false;
+        }
+        return $schema["type"] === "number";
+    }
+
+    public function typeAnnotation(): string
+    {
+        return "int|float";
+    }
+
+    public function typeHint(string $phpVersion): ?string
+    {
+        if (Semver::satisfies($phpVersion, "<8.0")) {
+            return null;
+        }
+
+        return "int|float";
+    }
+
+    public function generateTypeAssertionExpr(string $expr): string
+    {
+        return "is_int({$expr}) || is_float({$expr})";
+    }
+
+    public function generateInputMappingExpr(string $expr, bool $asserted = false): string
+    {
+        if ($asserted) {
+            return $expr;
+        }
+
+        return "str_contains({$expr}, '.') ? (float)({$expr}) : (int)({$expr})";
+    }
+
+}
