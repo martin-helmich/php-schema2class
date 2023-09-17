5fb504f2e0f8eaaa12a624c190208209258ed54f
Martin Helmich <m.helmich@mittwald.de>
2023-09-17 11:40:00 +0200

    Correctly typecast inputs for assoc arrays

--- src/Generator/Property/PrimitiveArrayProperty.php
+++ src/Generator/Property/PrimitiveArrayProperty.php
@@ -2,25 +2,19 @@
 declare(strict_types = 1);
 namespace Helmich\Schema2Class\Generator\Property;
 
+use Helmich\Schema2Class\Generator\GeneratorRequest;
 use Helmich\Schema2Class\Generator\SchemaToClass;
 
 class PrimitiveArrayProperty extends AbstractProperty
 {
     use TypeConvert;
 
+    private bool $isAssociativeArray;
+
     public static function canHandleSchema(array $schema): bool
     {
-        $itemSchema = null;
         $isAssociativeArray = isset($schema["additionalProperties"]) && is_array($schema["additionalProperties"]);
         $isArray = isset($schema["type"]) && $schema["type"] === "array";
-
-        if ($isAssociativeArray) {
-            $itemSchema = $schema["additionalProperties"];
-        }
-
-        if ($isArray) {
-            $itemSchema = $schema["items"];
-        }
 
         if (!$isArray && !$isAssociativeArray) {
             return false;
@@ -29,6 +23,12 @@ class PrimitiveArrayProperty extends AbstractProperty
         return !ObjectArrayProperty::canHandleSchema($schema);
     }
 
+    public function __construct(string $key, array $schema, GeneratorRequest $generatorRequest)
+    {
+        parent::__construct($key, $schema, $generatorRequest);
+
+        $this->isAssociativeArray = isset($schema["additionalProperties"]) && is_array($schema["additionalProperties"]);
+    }
 
     public function isComplex(): bool
     {
@@ -45,12 +45,12 @@ class PrimitiveArrayProperty extends AbstractProperty
     public function typeAnnotation(): string
     {
         if (isset($this->schema["items"])) {
-            [$annot, $hint] = $this->phpPrimitiveForSchemaType($this->schema["items"]);
+            [$annot,] = $this->phpPrimitiveForSchemaType($this->schema["items"]);
             return $annot . "[]";
         }
 
         if (isset($this->schema["additionalProperties"]) && is_array($this->schema["additionalProperties"])) {
-            [$annot, $hint] = $this->phpPrimitiveForSchemaType($this->schema["additionalProperties"]);
+            [$annot,] = $this->phpPrimitiveForSchemaType($this->schema["additionalProperties"]);
             return $annot . "[]";
         }
 
@@ -66,5 +66,14 @@ class PrimitiveArrayProperty extends AbstractProperty
     {
         return "is_array({$expr})";
     }
+
+    public function generateInputMappingExpr(string $expr, bool $asserted = false): string
+    {
+        if ($this->isAssociativeArray) {
+            return "(array){$expr}";
+        }
+        return parent::generateInputMappingExpr($expr, $asserted);
+    }
+
 
 }
