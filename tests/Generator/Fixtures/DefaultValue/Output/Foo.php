2a4810ee7ca02a829fd6ca3f8f2b076e5bce77a1
Martin Helmich <m.helmich@mittwald.de>
2024-09-04 16:21:07 +0200

    Add failing test case for resetting default values

---
+++ tests/Generator/Fixtures/DefaultValue/Output/Foo.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ns\DefaultValue;
+
+class Foo
+{
+    /**
+     * Schema used to validate input for creating instances of this class
+     *
+     * @var array
+     */
+    private static array $schema = [
+        'required' => [
+            
+        ],
+        'properties' => [
+            'limit' => [
+                'type' => 'integer',
+                'default' => 10000,
+                'minimum' => 1,
+            ],
+            'skip' => [
+                'type' => 'integer',
+                'default' => 0,
+            ],
+        ],
+    ];
+
+    /**
+     * @var int
+     */
+    private int $limit = 10000;
+
+    /**
+     * @var int
+     */
+    private int $skip = 0;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit() : int
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSkip() : int
+    {
+        return $this->skip;
+    }
+
+    /**
+     * @param int $limit
+     * @return self
+     */
+    public function withLimit(int $limit) : self
+    {
+        $validator = new \JsonSchema\Validator();
+        $validator->validate($limit, static::$schema['properties']['limit']);
+        if (!$validator->isValid()) {
+            throw new \InvalidArgumentException($validator->getErrors()[0]['message']);
+        }
+
+        $clone = clone $this;
+        $clone->limit = $limit;
+
+        return $clone;
+    }
+
+    /**
+     * @return self
+     */
+    public function withoutLimit() : self
+    {
+        $clone = clone $this;
+        $clone->limit = 10000;
+
+        return $clone;
+    }
+
+    /**
+     * @param int $skip
+     * @return self
+     */
+    public function withSkip(int $skip) : self
+    {
+        $validator = new \JsonSchema\Validator();
+        $validator->validate($skip, static::$schema['properties']['skip']);
+        if (!$validator->isValid()) {
+            throw new \InvalidArgumentException($validator->getErrors()[0]['message']);
+        }
+
+        $clone = clone $this;
+        $clone->skip = $skip;
+
+        return $clone;
+    }
+
+    /**
+     * @return self
+     */
+    public function withoutSkip() : self
+    {
+        $clone = clone $this;
+        $clone->skip = 0;
+
+        return $clone;
+    }
+
+    /**
+     * Builds a new instance from an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $validate Set this to false to skip validation; use at own risk
+     * @return Foo Created instance
+     * @throws \InvalidArgumentException
+     */
+    public static function buildFromInput(array|object $input, bool $validate = true) : Foo
+    {
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        if ($validate) {
+            static::validateInput($input);
+        }
+
+        $limit = 10000;
+        if (isset($input->{'limit'})) {
+            $limit = (int)($input->{'limit'});
+        }
+        $skip = 0;
+        if (isset($input->{'skip'})) {
+            $skip = (int)($input->{'skip'});
+        }
+
+        $obj = new self();
+        $obj->limit = $limit;
+        $obj->skip = $skip;
+        return $obj;
+    }
+
+    /**
+     * Converts this object back to a simple array that can be JSON-serialized
+     *
+     * @return array Converted array
+     */
+    public function toJson() : array
+    {
+        $output = [];
+        if (isset($this->limit)) {
+            $output['limit'] = $this->limit;
+        }
+        if (isset($this->skip)) {
+            $output['skip'] = $this->skip;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Validates an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $return Return instead of throwing errors
+     * @return bool Validation result
+     * @throws \InvalidArgumentException
+     */
+    public static function validateInput(array|object $input, bool $return = false) : bool
+    {
+        $validator = new \JsonSchema\Validator();
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        $validator->validate($input, static::$schema);
+
+        if (!$validator->isValid() && !$return) {
+            $errors = array_map(function(array $e): string {
+                return $e["property"] . ": " . $e["message"];
+            }, $validator->getErrors());
+            throw new \InvalidArgumentException(join(", ", $errors));
+        }
+
+        return $validator->isValid();
+    }
+
+    public function __clone()
+    {
+    }
+}
