9027cc4f596b920a6bc736862e784731cf985ae9
simivar <simivar@gmail.com>
2024-11-11 22:04:25 +0100

    test: add collector

--- tests/Generator/SchemaDefinitionsCollectorTest.php
+++ tests/Generator/SchemaDefinitionsCollectorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Helmich\Schema2Class\Generator;
 
+use Helmich\Schema2Class\Generator\Definitions\Definition;
 use Helmich\Schema2Class\Generator\Definitions\DefinitionsCollector;
 use Helmich\Schema2Class\Spec\SpecificationOptions;
 use Helmich\Schema2Class\Spec\ValidatedSpecificationFilesItem;
@@ -52,8 +53,32 @@ final class SchemaDefinitionsCollectorTest extends TestCase
             opts: new SpecificationOptions(),
         ));
         $definitions = $definitionsGenerator->collect($schema);
+        $definitionsArray = iterator_to_array($definitions);
 
-        $this->assertSame([], []);
-        //$this->assertSame([], iterator_to_array($definitions));
+        $this->assertCount(3, $definitionsArray);
+
+        $this->assertArrayHasKey('#/$defs/address', $definitionsArray);
+        $this->assertArrayHasKey('#/definitions/address', $definitionsArray);
+        $this->assertArrayHasKey('#/definitions/address/$defs/name', $definitionsArray);
+
+        $this->assertInstanceOf(Definition::class, $definitionsArray['#/$defs/address']);
+        $this->assertInstanceOf(Definition::class, $definitionsArray['#/definitions/address']);
+        $this->assertInstanceOf(Definition::class, $definitionsArray['#/definitions/address/$defs/name']);
+
+        $this->assertSame('TargetNamespace\Defs', $definitionsArray['#/$defs/address']->namespace);
+        $this->assertSame('TargetNamespace\Definitions', $definitionsArray['#/definitions/address']->namespace);
+        $this->assertSame('TargetNamespace\Definitions\Address\Defs', $definitionsArray['#/definitions/address/$defs/name']->namespace);
+
+        $this->assertSame('targetDirectory/Defs', $definitionsArray['#/$defs/address']->directory);
+        $this->assertSame('targetDirectory/Definitions', $definitionsArray['#/definitions/address']->directory);
+        $this->assertSame('targetDirectory/Definitions/Address/Defs', $definitionsArray['#/definitions/address/$defs/name']->directory);
+
+        $this->assertSame('TargetNamespace\Defs\Address', $definitionsArray['#/$defs/address']->classFQN);
+        $this->assertSame('TargetNamespace\Definitions\Address', $definitionsArray['#/definitions/address']->classFQN);
+        $this->assertSame('TargetNamespace\Definitions\Address\Defs\Name', $definitionsArray['#/definitions/address/$defs/name']->classFQN);
+
+        $this->assertSame('Address', $definitionsArray['#/$defs/address']->className);
+        $this->assertSame('Address', $definitionsArray['#/definitions/address']->className);
+        $this->assertSame('Name', $definitionsArray['#/definitions/address/$defs/name']->className);
     }
 }
